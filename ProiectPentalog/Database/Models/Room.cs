49a371371b4ce0bc0662aa499f164526fdb4a9a9
DieLuftwaffe <36141176+DieLuftwaffe@users.noreply.github.com>
2018-07-16 13:32:53 +0300

    Add database

---
+++ ProiectPentalog/Database/Models/Room.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProiectPentalog.Database.Models
+{
+    public class Room
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public virtual List<Reservation> Reservations { get; set; }
+    }
+}
--- ProiectPentalog/Global.asax.cs
+++ ProiectPentalog/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ProiectPentalog.Database;
+using ProiectPentalog.Database.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +14,18 @@ namespace ProiectPentalog
     {
         protected void Application_Start()
         {
+            using (RoomsDbContext ctx = new RoomsDbContext())
+            {
+                ctx.Rooms.Add(new Room() { Name = "Test" });
+                ctx.SaveChanges();
+            }
+
+            using (RoomsDbContext ctx = new RoomsDbContext())
+            {
+                int x = ctx.Rooms.Count();
+                int y = 0;
+            }
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
