a2b5f61d76d5cb1642c2285972e7d15a6c057e47
ElenaDobree <dobre.elena44@yahoo.com>
2018-07-17 12:29:07 +0300

    Lista de sali

--- ProiectPentalog/Global.asax.cs
+++ ProiectPentalog/Global.asax.cs
@@ -14,18 +14,6 @@ namespace ProiectPentalog
     {
         protected void Application_Start()
         {
-            using (RoomsDbContext ctx = new RoomsDbContext())
-            {
-                ctx.Rooms.Add(new Room() { Name = "Test" });
-                ctx.SaveChanges();
-            }
-
-            using (RoomsDbContext ctx = new RoomsDbContext())
-            {
-                int x = ctx.Rooms.Count();
-                int y = 0;
-            }
-
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
