8dfd62e70de506d1e0095d8afa6dee91b1a67f8e
DieLuftwaffe <36141176+DieLuftwaffe@users.noreply.github.com>
2018-07-25 12:57:10 +0300

    Interfata (mai e de lucrat)

--- ProiectPentalog/App_Start/BundleConfig.cs
+++ ProiectPentalog/App_Start/BundleConfig.cs
@@ -11,6 +11,13 @@ namespace ProiectPentalog
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/rest")
+                .Include("~/Scripts/headroom.min.js")
+                .Include("~/Scripts/html5shiv.js")
+                .Include("~/Scripts/jQuery.headroom.min.js")
+                .Include("~/Scripts/template.js")
+                .Include("~/Scripts/respond.min.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
@@ -22,9 +29,8 @@ namespace ProiectPentalog
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css")
+                .Include("~/Content/css/bootstrap-theme.css", "~/Content/css/bootstrap.min.css", "~/Content/css/font-awesome.min.css", "~/Content/css/main.css"));
         }
     }
 }
